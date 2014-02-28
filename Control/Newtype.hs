1249485a75d6c01cab13b913f01a15820de96410
João Cristóvão <jmacristovao@gmail.com>
2014-02-28 16:59:57 +0000

    Removed class restriction and orphan instances

--- Control/Newtype.hs
+++ Control/Newtype.hs
@@ -16,7 +16,9 @@ The 'Newtype' typeclass and related functions: 'op', 'ala', 'ala'', 'under'. Pri
 
 NB: 'Data.Foldable.foldMap' is a generalized @mconcatMap@ which is a generalized @concatMap@.
 
-This package includes 'Newtype' instances for all the (non-GHC\/foreign) newtypes in base (as seen in the examples). However, there are neat things you can do with this with /any/ newtype and you should definitely define your own 'Newtype' instances for the power of this library. For example, see @ala Cont traverse@, with the proper 'Newtype' instance for Cont.
+This package includes 'Newtype' instances for all the (non-GHC\/foreign) newtypes in base (as seen in the examples).
+However, there are neat things you can do with this with /any/ newtype and you should definitely define your own 'Newtype' instances for the power of this library.
+For example, see @ala Cont traverse@, with the proper 'Newtype' instance for Cont.
 -}
 module Control.Newtype ( Newtype(..), op, ala, ala', under, over, underF, overF ) where
 
@@ -56,26 +58,22 @@ instance GNewtype (D1 d (C1 c (S1 s (K1 i a)))) where
 
 -- | As long as the type @n@ is an instance of Generic, you can create an instance
 -- with just @instance Newtype n@
-class (O n ~ GO (Rep n)) => Newtype n where
+class Newtype n where
   type O n :: *
   type O n = GO (Rep n)
 
   pack   :: O n -> n
-  default pack :: (Generic n, GNewtype (Rep n)) => O n -> n
+  default pack :: (Generic n, GNewtype (Rep n), O n ~ GO (Rep n)) => O n -> n
   pack = to . gpack
 
   unpack :: n -> O n
-  default unpack :: (Generic n, GNewtype (Rep n)) => n -> O n
+  default unpack :: (Generic n, GNewtype (Rep n), O n ~ GO (Rep n)) => n -> O n
   unpack = gunpack . from
 
-{-
-This would be nice, but it breaks in odd ways with GHC < 7.
-Oh, and it also makes the instances an extra line longer. :(
-class Newtype n where
-  type Orig n
-  pack :: Orig n -> n
-  unpack :: n -> Orig n
--}
+instance Newtype All where
+  type O All = Bool
+  pack = All
+  unpack (All x) = x
 
 -- |
 -- This function serves two purposes:
@@ -83,61 +81,94 @@ class Newtype n where
 -- 1. Giving you the unpack of a newtype without you needing to remember the name.
 --
 -- 2. Showing that the first parameter is /completely ignored/ on the value level, meaning the only reason you pass in the constructor is to provide type information. Typeclasses sure are neat.
-op :: (Newtype n, o ~ GO (Rep n)) => (o -> n) -> n -> o
+op :: (Newtype n,o ~ O n ) => (o -> n) -> n -> o
 op _ = unpack
 
 -- | The workhorse of the package. Given a pack and a \"higher order function\", it handles the packing and unpacking, and just sends you back a regular old function, with the type varying based on the hof you passed.
 --
 -- The reason for the signature of the hof is due to 'ala' not caring about structure. To illustrate why this is important, another function in this package is 'under'. It is not extremely useful; @under2@ might be more useful (with e.g., @mappend@), but then we already digging the trench of \"What about @under3@? @under4@?\". The solution utilized here is to just hand off the \"packer\" to the hof. That way your structure can be imposed in the hof, whatever you may want it to be (e.g., List, Traversable).
-ala :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n')) => (o -> n) -> ((o -> n) -> b -> n') -> (b -> o')
+ala :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ O n', o ~ O n, o' ~ GO (Rep n')) => (o -> n) -> ((o -> n) -> b -> n') -> (b -> o')
 ala pa hof = ala' pa hof id
 
 -- | This is the original function seen in Conor McBride's work. The way it differs from the 'ala' function in this package, is that it provides an extra hook into the \"packer\" passed to the hof. However, this normally ends up being @id@, so 'ala' wraps this function and passes @id@ as the final parameter by default. If you want the convenience of being able to hook right into the hof, you may use this function.
-ala' :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n')) => (o -> n) -> ((a -> n) -> b -> n') -> (a -> o) -> (b -> o')
+ala' :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), o' ~ O n', o ~ O n) => (o -> n) -> ((a -> n) -> b -> n') -> (a -> o) -> (b -> o')
 ala' _ hof f = unpack . hof (pack . f)
 
 -- | A very simple operation involving running the function \'under\' the newtype. Suffers from the problems mentioned in the 'ala' function's documentation.
-under :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n')) => (o -> n) -> (n -> n') -> (o -> o')
+under :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), o' ~ O n', o ~ O n) => (o -> n) -> (n -> n') -> (o -> o')
 under _ f = unpack . f . pack
 
 -- | The opposite of 'under'. I.e., take a function which works on the underlying types, and switch it to a function that works on the newtypes.
-over :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n')) => (o -> n) -> (o -> o') -> (n -> n')
+over :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), o' ~ O n', o ~ O n) => (o -> n) -> (o -> o') -> (n -> n')
 over _ f = pack . f . unpack
 
 -- | 'under' lifted into a Functor.
-underF :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), Functor f) => (o -> n) -> (f n -> f n') -> (f o -> f o')
+underF :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), o' ~ O n', o ~ O n, Functor f) => (o -> n) -> (f n -> f n') -> (f o -> f o')
 underF _ f = fmap unpack . f . fmap pack
 
 -- | 'over' lifted into a Functor.
-overF :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), Functor f) => (o -> n) -> (f o -> f o') -> (f n -> f n')
+overF :: (Newtype n, o ~ GO (Rep n), Newtype n', o' ~ GO (Rep n'), o' ~ O n', o ~ O n, Functor f) => (o -> n) -> (f o -> f o') -> (f n -> f n')
 overF _ f = fmap pack . f . fmap unpack
 
--- why aren't those defined in base?
-deriving instance Generic All
-deriving instance Generic Any
-deriving instance Generic (Sum a)
-deriving instance Generic (Product a)
-deriving instance Generic (Kleisli m a b)
-deriving instance Generic (WrappedMonad m a)
-deriving instance Generic (WrappedArrow a b c)
-deriving instance Generic (ZipList a)
-deriving instance Generic (Const a x)
-deriving instance Generic (Endo a)
-deriving instance Generic (First a)
-deriving instance Generic (Last a)
-deriving instance Generic (ArrowMonad a b)
+instance Newtype Any where
+  type O Any = Bool
+  pack = Any
+  unpack (Any x) = x
 
--- original instances
-instance Newtype All
-instance Newtype Any
-instance Newtype (Sum a)
-instance Newtype (Product a)
-instance Newtype (Kleisli m a b)
-instance Newtype (WrappedMonad m a)
-instance Newtype (WrappedArrow a b c)
-instance Newtype (ZipList a)
-instance Newtype (Const a x)
-instance Newtype (Endo a)
-instance Newtype (First a)
-instance Newtype (Last a)
-instance ArrowApply a => Newtype (ArrowMonad a b)
+instance Newtype (Sum a) where
+  type O (Sum a) = a
+  pack = Sum
+  unpack (Sum a) = a
+
+instance Newtype (Product a) where
+  type O (Product a) = a
+  pack = Product
+  unpack (Product a) = a
+
+instance Newtype (First a) where
+  type O (First a) = Maybe a
+  pack = First
+  unpack (First a) = a
+
+instance Newtype (Last a) where
+  type O (Last a) = Maybe a
+  pack = Last
+  unpack (Last a) = a
+
+instance Newtype (Endo a) where
+  type O (Endo a) = (a -> a)
+  pack = Endo
+  unpack (Endo a) = a
+
+instance Newtype (ZipList a) where
+  type O (ZipList a) = [a]
+  pack = ZipList
+  unpack (ZipList a) = a
+
+instance Newtype (Const a x) where
+  type O (Const a x) = a
+  pack = Const
+  unpack (Const a) = a
+
+instance Newtype (Kleisli m a b) where
+  type O (Kleisli m a b) = a -> m b
+  pack = Kleisli
+  unpack (Kleisli a) = a
+
+instance Newtype (WrappedMonad m a) where
+  type O (WrappedMonad m a) = m a
+  pack = WrapMonad
+  unpack (WrapMonad a) = a
+
+instance Newtype (WrappedArrow a b c) where
+  type O (WrappedArrow a b c) = a b c
+  pack = WrapArrow
+  unpack (WrapArrow a) = a
+
+instance Newtype (ArrowMonad a b) where
+  type O (ArrowMonad a b) = a () b
+  pack = ArrowMonad
+  unpack (ArrowMonad a) = a
+
+
+
