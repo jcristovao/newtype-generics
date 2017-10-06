d961253de4f0b9e60e5904bfcf8db3ef5006110f
Simon Jakobi <simon.jakobi@gmail.com>
2017-10-06 04:38:10 +0200

    Add benchmark
    Results with GHC-8.2.1 are below. `ala` performs much worse with GHC-8.0.1.
    
    benchmarking [1..5 :: Int]/foldMap/ala MySumDerive
    time                 40.44 ns   (40.36 ns .. 40.58 ns)
                         1.000 R²   (1.000 R² .. 1.000 R²)
    mean                 40.27 ns   (40.19 ns .. 40.43 ns)
    std dev              372.9 ps   (217.4 ps .. 584.6 ps)
    
    benchmarking [1..5 :: Int]/foldMap/ala MySumManual
    time                 40.03 ns   (39.97 ns .. 40.12 ns)
                         1.000 R²   (1.000 R² .. 1.000 R²)
    mean                 39.85 ns   (39.80 ns .. 39.91 ns)
    std dev              184.9 ps   (144.8 ps .. 243.5 ps)
    
    benchmarking [1..5 :: Int]/foldMap/manual wrap & unwrap
    time                 42.71 ns   (42.63 ns .. 42.80 ns)
                         1.000 R²   (1.000 R² .. 1.000 R²)
    mean                 42.55 ns   (42.47 ns .. 42.66 ns)
    std dev              300.7 ps   (165.8 ps .. 478.0 ps)
    
    benchmarking [1..5 :: Int]/foldMap/coerce
    time                 44.07 ns   (44.02 ns .. 44.13 ns)
                         1.000 R²   (1.000 R² .. 1.000 R²)
    mean                 43.92 ns   (43.86 ns .. 44.00 ns)
    std dev              237.5 ps   (174.0 ps .. 314.8 ps)
    
    benchmarking [1..5 :: Int]/Prelude.sum
    time                 36.13 ns   (35.40 ns .. 37.33 ns)
                         0.998 R²   (0.995 R² .. 1.000 R²)
    mean                 35.49 ns   (35.31 ns .. 36.09 ns)
    std dev              983.4 ps   (330.4 ps .. 2.033 ns)
    variance introduced by outliers: 44% (moderately inflated)

---
+++ bench/main.hs
@@ -0,0 +1,61 @@
+{-# language DeriveGeneric #-}
+{-# language TypeFamilies #-}
+module Main where
+
+import Criterion
+import Criterion.Main
+import Control.Newtype
+import Data.Coerce
+import Data.Semigroup
+import GHC.Generics
+
+newtype MySumDerive = MySumDerive Int
+  deriving (Generic)
+instance Newtype MySumDerive
+instance Semigroup MySumDerive where
+  MySumDerive x <> MySumDerive y = MySumDerive (x + y)
+instance Monoid MySumDerive where
+  mappend = (<>)
+  mempty = MySumDerive 0
+
+newtype MySumManual = MySumManual Int
+instance Newtype MySumManual where
+  type O MySumManual = Int
+  pack = MySumManual
+  unpack (MySumManual x) = x
+instance Semigroup MySumManual where
+  MySumManual x <> MySumManual y = MySumManual (x + y)
+instance Monoid MySumManual where
+  mappend = (<>)
+  mempty = MySumManual 0
+
+mySumDerive :: [Int] -> Int
+mySumDerive xs = ala MySumDerive foldMap xs
+
+mySumManual :: [Int] -> Int
+mySumManual xs = ala MySumManual foldMap xs
+
+mySumOldschool :: [Int] -> Int
+mySumOldschool xs = s
+  where MySumDerive s = foldMap MySumDerive xs
+
+mySumCoerce :: [Int] -> Int
+mySumCoerce xs = coerce (foldMap coerce xs :: MySumDerive)
+
+preludeSum :: [Int] -> Int
+preludeSum xs = sum xs
+
+main :: IO ()
+main = defaultMain [
+    env (return [1..5 :: Int]) $ \ns ->
+      let bench' s f = bench s (whnf f ns)
+      in bgroup "[1..5 :: Int]"
+        [ bgroup "foldMap"
+            [ bench' "ala MySumDerive" mySumDerive
+            , bench' "ala MySumManual" mySumManual
+            , bench' "manual wrap & unwrap" mySumOldschool
+            , bench' "coerce" mySumCoerce
+            ]
+        , bench' "Prelude.sum" preludeSum
+        ]
+  ]
